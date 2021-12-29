082ef2f3f56fb428adccf693798d93d692938406
Chris Wiederspan <chris@wiederspan.com>
2021-12-29 12:56:10 -0700

    Update hello_world.py
    Updated start date

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -6,7 +6,7 @@ from airflow.operators.python_operator import PythonOperator
 def print_hello():
     return 'Hello world from first Airflow DAG!'
 
-dag = DAG('hello_world', description='Hello World DAG', schedule_interval='10 * * * *', start_date=datetime(2017, 3, 20), catchup=False)
+dag = DAG('hello_world', description='Hello World DAG', schedule_interval='10 * * * *', start_date=datetime(2021, 12, 1), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
